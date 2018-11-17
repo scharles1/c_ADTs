82156de14d0941212f1237665877be798ec9beaa
scharles1 <sethec@stanford.edu>
2018-11-17 14:55:39 -0800

    basic list test structure implementation

--- test/TestList.c
+++ test/TestList.c
@@ -1,9 +1,16 @@
 #include "List.h"
 #include "unity.h"
 
+static void 
+test_list_init (void)
+{
+	TEST_ASSERT_MESSAGE (0 == 1, "failed list initialization");
+}
+
 int 
 main(void)
 {
 	UNITY_BEGIN ();
+	RUN_TEST (test_list_init);
 	return UNITY_END ();
 }
