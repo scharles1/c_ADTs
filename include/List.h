5f8920415b3e0520c60d25bade0fbb6481c420a1
scharles1 <sethec@stanford.edu>
2018-11-17 20:34:20 -0800

    skeleton API for list

--- include/List.h
+++ include/List.h
@@ -13,21 +13,60 @@
 /**
  * Type: list
  * ------------------------------------------------------
- * The intention with this type is to hide the internals of the vector struct
- * by defining the vector_implementation struct in the vector.c file. This 
+ * The intention with this type is to hide the internals of the list struct
+ * by defining the list_implementation struct in the LIST.c file. This 
  * requires clients to use the prescribed API
  */
 typedef struct list_implementation list;
 
 /**
- * Function: vector_init
- * Usage: vector *v = vector_init (sizeof(int), 10, NULL)
+ * Type: list_elem
  * ------------------------------------------------------
- * Creates a new empty vector and returns a pointer to it.
- *
- * Asserts: zero elemsz, allocation failure
- * Assumes: cleanup fn is valid
+ * The intention with this type is to hide the internals of the list_elem
+ * struct by defining the list_elem_implementation struct in the LIST.c file. 
+ * This requires clients to use the prescribed API
  */
-list *list_init (size_t elem_sz, size_t capacity_hint, elem_destroy_fn fn);
+typedef struct list_elem_implementation list_elem;
+
+/**
+ * Function: list_init_static
+ * Usage: list l;
+ *        list_init (&l);
+ * ------------------------------------------------------
+ * Creates a new empty list. The space for the list object is allocated by the
+ * caller.
+ */
+void list_init_static (list *l, elem_destroy_fn fn);
+
+/**
+ * Function: list_init
+ * Usage: list *v = list_init (void)
+ * ------------------------------------------------------
+ * Creates a new empty list and returns it to the caller
+ */
+list *list_init (elem_destroy_fn fn);
+
+/**
+ * Function: list_destroy
+ * Usage: list_destroy (l)
+ * ------------------------------------------------------
+ * Destroys and frees all memory associated with list
+ *
+ * Asserts: null pointer
+ * Assumes: valid initialized vector pointer
+ */
+void list_destroy (list *l);
+
+void list_push_front (list *l, void *new_node);
+
+void list_push_back (list *l, void *new_node);
+
+void *list_front (list *l);
+
+void *list_back (list *l);
+
+void list_pop_front (list *l);
+
+void list_pop_back (list *l);
 
 #endif /* LIST_H */
--- src/List.c
+++ src/List.c
@@ -3,3 +3,104 @@
  * Author: Seth Charles
  * ----------------------
  */
+#include "List.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+#define GET_PTR_ELEM(V, INDEX) ((char *)(V->elems) + ((INDEX) * (V->elem_sz)))
+
+/**
+ * Struct: list_implementation
+ * ----------------------------------
+ * The private list implementation
+ *
+ * field 
+ */
+struct list_implementation
+{
+	list_elem *head;
+	list_elem *tail;
+	size_t n_elems;
+	elem_destroy_fn elem_destroy;
+	bool alloc_static;
+};
+
+/**
+ * Struct: list_elem_implementation
+ * ----------------------------------
+ * The private list_elem implementation
+ *
+ * field 
+ */
+struct list_elem_implementation
+{
+	list_elem *next;
+	list_elem *prev;
+};
+
+/**
+ * Function: list_init_static
+ * ------------------------------------------------------
+ */
+void 
+list_init_static (list *l, elem_destroy_fn fn)
+{
+	assert (l != NULL);
+
+	l->head = l->tail = NULL;
+	l->n_elems = 0;
+	l->elem_destroy = fn;
+	l->alloc_static = true;
+}
+
+/**
+ * Function: list_init
+ * ------------------------------------------------------
+ */
+list *
+list_init (elem_destroy_fn fn)
+{
+	list *l = malloc (sizeof (list));
+	assert (l != NULL);
+
+	l->head = l->tail = NULL;
+	l->n_elems = 0;
+	l->elem_destroy = fn;
+	l->alloc_static = false;
+
+	return l;
+}
+
+/**
+ * Function: list_destroy
+ * Usage: list_destroy (l)
+ * ------------------------------------------------------
+ * Destroys and frees all memory associated with list
+ *
+ * Asserts: null pointer
+ * Assumes: valid initialized vector pointer
+ */
+void
+list_destroy (list *l)
+{
+	assert (l != NULL);
+
+	list_elem *l_elem, *l_elem_next;
+
+	if (l->elem_destroy)
+	{
+		l_elem = l->head;
+		while (l_elem)
+		{
+			l_elem_next = l_elem->next;
+			l->elem_destroy (l_elem);
+			l_elem = l_elem_next;
+		}
+	}
+
+	if (!l->alloc_static)
+	{
+		free (l);
+	}
+}
